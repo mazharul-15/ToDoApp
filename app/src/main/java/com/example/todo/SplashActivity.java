4cd780fe38f656fcd2160cb854470f814dc718e4
mazharul-15 <mazharulislamhstu15@gmail.com>
2024-06-02 09:21:55 +0600

    app launcher icon & material design added

--- app/src/main/java/com/example/todo/SplashActivity.java
+++ app/src/main/java/com/example/todo/SplashActivity.java
@@ -13,15 +13,17 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        //Handler handler = new Handler();
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                //startActivity(intent);
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 finish();
             }
-        }, 4000);
+        }, 3000);
     }
 }
